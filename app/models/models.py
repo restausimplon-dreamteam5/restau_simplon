e1a5ec15d079acd4f6d107ca22eb272719f02598
Cesar Gattano <cesar.gattano@gmail.com>
2025-08-06 13:48:03 +0200

    feat: :sparkles: Ajout des modèles MenuCategory et MenuItem
    Ajouté dans models.py, MenuItem sélectionné pour la création d'une table dans SQL

--- app/models/models.py
+++ app/models/models.py
@@ -1,10 +1,41 @@
 # Import
+import uuid
+from sqlmodel import Field, SQLModel, Column, Enum as smEnum
+from decimal import Decimal
+from enum import Enum
 
 
 # User
 
 
 # Article
+
+
+class MenuCategory(str, Enum):
+    "Énumération des catégories dans le menu"
+
+    starter = "starter"
+    main_course = "main course"
+    dessert = "dessert"
+
+
+class MenuItem(SQLModel, table=True):
+    """Modèle d'article de menu pour la base de données"""
+
+    __tablename__ = "menu_items"
+
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    name: str = Field(..., unique=True, index=True, max_length=100)
+    price: Decimal = Field(..., max_digits=8, decimal_places=2)
+    category: MenuCategory = Field(...)
+    # category: MenuCategory = Field(
+    #     sa_column=Column(
+    #         smEnum(MenuCategory, name="category", create_type=True),
+    #         nullable=False,
+    #     ),
+    # )
+    description: str | None = Field(None)
+    stock: int = Field(default=0, ge=0)
 
 
 # Order
