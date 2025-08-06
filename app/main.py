d464817b6e1d9258d6de7d1ee4e7650c5878a3a8
Cesar Gattano <cesar.gattano@gmail.com>
2025-08-06 22:14:29 +0200

    feat: :bricks: Déplacement create_db_and_tables depuis main.py vers database.py
    + Nettoyage des imports de main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,6 @@
-from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-from app.schemas.schemas import MenuItemCreate, MenuItemOut
-from app.models.models import MenuItem
-from .database import engine
-from .deps import SessionDep
+from fastapi import FastAPI
+from .database import create_db_and_tables
 from app.routes import menu_item
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
