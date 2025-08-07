976d21e8ec9cf0e2fb07be24febac588e10cb960
Cesar Gattano <cesar.gattano@gmail.com>
2025-08-07 12:23:26 +0200

    feat: :sparkles: Nettoyage de la table menu_item dans db_init pour pouvoir éxecuter plusieurs fois le script

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ sys.path.append(os.getcwd())
 from app.models.models import MenuItem
 from app.schemas.schemas import MenuItemCreate
 from app.crud.menu_items import create_menu_item
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import create_engine, select, SQLModel, Session
 import json
 
 sqlite_file_name = "database.db"
@@ -20,20 +20,26 @@ def create_db_and_tables():
 
 def db_init():
     """Ajoute du contenu aléatoire dans la base de données pour les tests.
-    
+
     **Attention**: la base de données doit être à jour des migrations alembiques"""
 
-    with open('app/menu_items.json', 'r') as file:
+    # Récupération des données générées par chatGPT
+    with open("app/menu_items.json", "r") as file:
         menu_items = json.load(file)
 
     with Session(engine) as session:
+        # Nettoyage complet de la table menu_items
+        all_menu_items = session.exec(select(MenuItem)).all()
+        for menu_item in all_menu_items:
+            session.delete(menu_item)
+        session.commit()
+
+        # Mise en base des informations de menu_items.json
         for menu_item in menu_items:
             menu_item_create = MenuItemCreate(**menu_item)
             menu_item_db = MenuItem(**menu_item_create.model_dump())
             if create_menu_item(session, menu_item_db):
-                print(
-                    f"Added: {menu_item_db.name} in category {menu_item_db.category}"
-                )
+                print(f"Added: {menu_item_db.name} in category {menu_item_db.category}")
 
 
 if __name__ == "__main__":
