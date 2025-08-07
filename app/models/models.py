8d40f84369f22f9227d39df57412c0f8e8c6ebba
Thomas Le Floch <t.lefloch53@gmail.com>
2025-08-07 15:44:44 +0200

    feat: modèle Permission et Role

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,6 @@
 # Import
 import uuid
-from sqlmodel import Field, SQLModel, Column, Enum as smEnum
+from sqlmodel import Field, Relationship, SQLModel, Column, Enum as smEnum
 from decimal import Decimal
 from enum import Enum
 from datetime import datetime
@@ -8,18 +8,28 @@ from pydantic import EmailStr
 
 
 # User
+class Permission(SQLModel, table=True):
+    user_id: uuid.UUID = Field(foreign_key="user_info.id", primary_key=True)
+    role_id: uuid.UUID = Field(foreign_key="role.id", primary_key=True)
+
+
+class Role(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    role: str
+
+
 class User(SQLModel, table=True):
     __tablename__ = "user_info"
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     first_name: str = Field(max_length=50)
     surname: str = Field(max_length=50)
-    # On a choisi de prendre en charges les numéro de type "0677889910". 
+    # On a choisi de prendre en charges les numéro de type "0677889910".
     # Pas d'espaces, pas prefix comme +33
     # Que des numeros français de 10 caractères
     # TODO: on pourrait verifier les prefix valables 06, 07, 01, 02, 03, 04,05 ...
     # TODO: regarder du côté de pydantic
-    # THINK: Est ce que le pattern à besoin d'être la ? 
+    # THINK: Est ce que le pattern à besoin d'être la ?
     # Il devrait être dans le schema d'entré. mais dans la db peut être pas
     phone: str = Field(
         min_length=10, max_length=10, schema_extra={"pattern": r"^[0-9]*$"}
@@ -29,6 +39,8 @@ class User(SQLModel, table=True):
     email: EmailStr = Field(index=True, unique=True, max_length=320)
     password: str
     created_at: datetime = Field(default_factory=datetime.now)
+
+    roles: list[Role] = Relationship(link_model=Permission)
 
 
 # Article
@@ -80,19 +92,17 @@ class Order(SQLModel, table=True):
 
     user_id: uuid.UUID = Field(foreign_key="user_info.id")
 
-# Detail commande 
+
+# Detail commande
 class OrderDetail(SQLModel, table=True):
     """Modèle de détail de commande pour la base de données"""
 
     __tablename__ = "order_detail"
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-   
+
     order_id: uuid.UUID = Field(foreign_key="order.id")
     item_id: uuid.UUID = Field(foreign_key="menu_item.id")
-    
+
     quantity: int = Field(gt=0, default=1)
-    unit_price: Decimal = Field(..., max_digits=8, decimal_places=2) 
-  
-
-
+    unit_price: Decimal = Field(..., max_digits=8, decimal_places=2)
