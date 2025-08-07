d80a91e8e08c3a39d888d2e24d35b1e1f8ebf7f9
Thomas Le Floch <t.lefloch53@gmail.com>
2025-08-07 18:10:28 +0200

    feat: route POST /login Authentification Bear Token. Renvoi un JWT avec de l'utilisateur

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from .database import create_db_and_tables
-from app.routes import menu_item, user, order
+from app.routes import menu_item, user, order, login, roles
 
 app = FastAPI()
 
@@ -8,6 +9,10 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+app.include_router(login.router)
 app.include_router(menu_item.router)
 app.include_router(user.router)
 app.include_router(order.router)
+app.include_router(roles.router)
---
+++ app/routes/login.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from sqlmodel import select
+
+from app.deps import SessionDep
+from app.models.models import User
+from app.schemas.schemas import Token, UserOut
+from fastapi.security import OAuth2PasswordRequestForm
+import jwt
+
+
+# to get a string like this run:
+# openssl rand -hex 32
+# TODO: gen 1 and put in env
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+router = APIRouter(prefix="/login", tags=["Login"])
+
+
+@router.post("/")
+def login(
+    logins: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep
+) -> Token:
+    hashed_password = logins.password  # TODO: hashé et salé le mdp
+    user = session.exec(
+        select(User)
+        .where(User.email == logins.username)
+        .where(User.password == hashed_password)
+    ).one()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Identifiants invalides",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    expiration_date = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    
+    user_roles= []
+    for role in user.roles:
+        user_roles.append(role.role)
+    
+    access_token = {"sub": logins.username, "roles": user_roles, "exp": expiration_date}
+    encoded_jwt = jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM)
+
+    return Token(access_token=encoded_jwt, token_type="bearer")
