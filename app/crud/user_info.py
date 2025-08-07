f27ac6dc2873a4ec90da52780d45a8ffe733cf33
Cesar Gattano <cesar.gattano@gmail.com>
2025-08-07 17:49:07 +0200

    feat: :sparkles: Création des fonctions create_*_in_db dans crud/ pour les 4 tables

---
+++ app/crud/user_info.py
@@ -0,0 +1,18 @@
+from sqlmodel import Session
+from app.models.models import User
+
+
+def create_user_info_in_db(session: Session, user_info_db: User) -> bool:
+    """Création d'un utilisateur dans la base de données.
+
+    Args:
+        session (Session): La session communicante avec la BDD
+        user_info (User): L'utilisateur à insérer en base.
+
+    Returns:
+        bool: Vrai si l'article a été mis en base
+    """
+    session.add(user_info_db)
+    session.commit()
+    session.refresh(user_info_db)
+    return True
