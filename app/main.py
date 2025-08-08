9878db5b74de462138e811feedbd95929e83aa24
Thomas Le Floch <t.lefloch53@gmail.com>
2025-08-08 14:07:55 +0200

    feat: cors sur localhost:8000
    Ne fonctionne pas ! je ne sais pas pourquoi. On verra plus tard

--- app/main.py
+++ app/main.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
 from .database import create_db_and_tables
 from app.routes import menu_item, user, order, login, roles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.on_event("startup")
 def on_startup():
