69f5438d95a65016a93d22b98056f966a8223ed5
Cesar Gattano <cesar.gattano@gmail.com>
2025-08-18 16:56:17 +0200

    feat: :sparkles: Ajout du unit test test_MenuItemCreate_init_too_long_name dans tests/units/schemas/menu_items.py

--- tests/units/schemas/test_menu_items.py
+++ tests/units/schemas/test_menu_items.py
@@ -4,6 +4,7 @@ import pytest
 sys.path.append(os.getcwd())
 from app.schemas.schemas import MenuItemCreate, MenuCategory
 from decimal import Decimal
+from pydantic import ValidationError
 
 
 # Définition d'un article de menu correct et complet
@@ -23,8 +24,8 @@ def correct_menu_item_example() -> dict:
     }
 
 
-def test_MenuItemCreate_init_correct(correct_menu_item_example):
-    # Act: Éxecution de la fonction testée
+def test_MenuItemCreate_init_correct(correct_menu_item_example: dict):
+    # Act: Exécution de la fonction testée
     menu_item = MenuItemCreate(**correct_menu_item_example)
 
     # Assert: Évaluation de la conformité du résultat
@@ -33,3 +34,18 @@ def test_MenuItemCreate_init_correct(correct_menu_item_example):
     assert menu_item.category == correct_menu_item_example["category"]
     assert menu_item.description == correct_menu_item_example["description"]
     assert menu_item.stock == correct_menu_item_example["stock"]
+
+
+def test_MenuItemCreate_init_too_long_name(correct_menu_item_example: dict):
+    # Arrange: État initial de l'environnement du test
+    correct_menu_item_example["name"] = (
+        "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890extra_characters"
+    )
+
+    # Act: Exécution de la fonction testée
+    with pytest.raises(ValidationError) as excinfo:
+        menu_item = MenuItemCreate(**correct_menu_item_example)
+        pass
+
+    # Assert: Évaluation de la conformité du résultat
+    assert "String should have at most 100 characters" in str(excinfo.value)
