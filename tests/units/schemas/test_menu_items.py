dfd716a6efdd51d9a05e7097856814725943b21e
Cesar Gattano <cesar.gattano@gmail.com>
2025-08-18 16:24:43 +0200

    feat: :sparkles: Ajout de la fixture correct_menu_item_example dans tests/units/schemacs/test_menu_items.py

--- tests/units/schemas/test_menu_items.py
+++ tests/units/schemas/test_menu_items.py
@@ -1,23 +1,35 @@
 import os, sys
+import pytest
 
 sys.path.append(os.getcwd())
 from app.schemas.schemas import MenuItemCreate, MenuCategory
 from decimal import Decimal
 
 
-def test_MenuItemCreate_init_correct():
+# Définition d'un article de menu correct et complet
+@pytest.fixture
+def correct_menu_item_example() -> dict:
+    """Retourne un article de menu type sous forme de dictionnaire.
+
+    Returns:
+        dict: Les données d'un article de menu type
+    """
+    return {
+        "name": "Tomatoe soup",
+        "price": Decimal("18.99"),
+        "category": "starter",
+        "description": "A warm soup made with tomatoes.",
+        "stock": 10,
+    }
+
+
+def test_MenuItemCreate_init_correct(correct_menu_item_example):
     # Act: Éxecution de la fonction testée
-    menu_item = MenuItemCreate(
-        name="Tomatoe soup",
-        price=Decimal("18.99"),
-        category=MenuCategory.starter,
-        description="A warm soup made with tomatoes.",
-        stock=10,
-    )
+    menu_item = MenuItemCreate(**correct_menu_item_example)
 
     # Assert: Évaluation de la conformité du résultat
-    assert menu_item.name == "Tomatoe soup"
-    assert menu_item.price == Decimal("18.99")
-    assert menu_item.category == MenuCategory.starter
-    assert menu_item.description == "A warm soup made with tomatoes."
-    assert menu_item.stock == 10
+    assert menu_item.name == correct_menu_item_example["name"]
+    assert menu_item.price == correct_menu_item_example["price"]
+    assert menu_item.category == correct_menu_item_example["category"]
+    assert menu_item.description == correct_menu_item_example["description"]
+    assert menu_item.stock == correct_menu_item_example["stock"]
