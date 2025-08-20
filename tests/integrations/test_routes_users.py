57dbc42479e2fa7754282234ee03c6ed121d8848
Thomas Le Floch <t.lefloch53@gmail.com>
2025-08-20 17:06:56 +0200

    ci: test qui échoue

--- tests/integrations/test_routes_users.py
+++ tests/integrations/test_routes_users.py
@@ -6,3 +6,7 @@ def test_site_available():
     print(os.environ["API_URL"])
     status_code = urllib.request.urlopen(f"{os.environ["API_URL"]}/docs").getcode()
     assert status_code == 200
+
+
+def test_qui_echoue():
+    assert 1 == 2
