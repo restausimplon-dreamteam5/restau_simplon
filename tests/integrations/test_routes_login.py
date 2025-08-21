f8452197224628998bc2c9bf7d7447c7cbb01df7
Cesar Gattano <cesar.gattano@gmail.com>
2025-08-21 09:20:03 +0200

    test: :white_check_mark: Test routes login et menu_items (par nom uniquement)

---
+++ tests/integrations/test_routes_login.py
@@ -0,0 +1,18 @@
+import requests
+import os
+
+
+def test_login():
+    # Act: Exécution de la route testée
+    req = requests.post(
+        f"{os.environ["API_URL"]}/login",
+        data={
+            "grant_type": "password",
+            "username": "admin@restau-simplon.com",
+            "password": "admin",
+            "client_id": "string",
+        },
+    )
+
+    # Assert: Évaluation de la conformité du résultat
+    assert req.status_code == 200
